6577d9c179a8cb48c0465307347fce7092f71023
Vladislav Sorokin <vladislav@proxylink.co>
2024-11-04 17:42:52 +0000

    fix: addParticipantsData

--- components/RequestActions/utils.ts
+++ components/RequestActions/utils.ts
@@ -8,26 +8,22 @@ export const addParticipantsData = (
   const { tenantType, tenantId, tenantName, email } = userData;
   const isProvider = tenantType === 'provider';
   const isParticipantAlready = isProvider
-    ? request.participants?.provider?.emails.includes(email)
-    : request.participants?.proxy?.emails.includes(email);
+    ? request.participants?.provider?.emails?.includes(email)
+    : request.participants?.proxy?.emails?.includes(email);
 
   const provider = {
     tenantId,
     tenantName,
-    emails: [
-      ...(isParticipantAlready
-        ? request.participants?.provider?.emails
-        : request.participants?.provider?.emails.concat(email)),
-    ],
+    emails: isParticipantAlready
+      ? (request.participants?.provider?.emails ?? [])
+      : [...(request.participants?.provider?.emails ?? []), email],
   };
   const proxy = {
     tenantId,
     tenantName,
-    emails: [
-      ...(isParticipantAlready
-        ? request.participants?.proxy?.emails
-        : request.participants?.proxy?.emails.concat(email)),
-    ],
+    emails: isParticipantAlready
+      ? (request.participants?.proxy?.emails ?? [])
+      : [...(request.participants?.proxy?.emails ?? []), email],
   };
 
   return {
