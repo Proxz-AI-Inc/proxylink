7260c5bf2dc458b5d979a1cd50bb939c30d43466
Vladislav Sorokin <vladislav@proxylink.co>
2024-11-11 17:21:52 +0000

    fix: auth

--- components/Login/ResetPasswordForm.tsx
+++ components/Login/ResetPasswordForm.tsx
@@ -1,21 +1,17 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import {
-  sendPasswordResetEmail,
-  confirmPasswordReset,
-  verifyPasswordResetCode,
-} from 'firebase/auth';
+import { confirmPasswordReset, verifyPasswordResetCode } from 'firebase/auth';
 import { auth } from '@/lib/firebase/config';
 import { useSearchParams } from 'next/navigation';
+import * as logger from '@/lib/logger/logger';
+import { parseErrorMessage } from '@/utils/general';
 
 const ResetPasswordForm: React.FC = () => {
   const searchParams = useSearchParams();
   const oobCode = searchParams.get('oobCode');
   const mode = searchParams.get('mode');
 
-  const [email, setEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [loading, setLoading] = useState(false);
@@ -46,30 +42,15 @@ const ResetPasswordForm: React.FC = () => {
       setLoading(true);
       await confirmPasswordReset(auth, oobCode as string, newPassword);
       setMessage('Password has been reset successfully.');
-      // Optionally redirect the user or provide further instructions
       setShowLoginButton(true);
     } catch (error) {
-      console.error('Error resetting password:', error);
+      logger.error('Error resetting password:', {
+        error: { message: parseErrorMessage(error) },
+        email: auth.currentUser?.email || 'unknown',
+        method: 'POST',
+        tenantId: auth.currentUser?.tenantId || 'unknown',
+      });
       setMessage('Error resetting password. Please try again.');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleSendResetEmail = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!email) {
-      setMessage('Please enter your email address.');
-      return;
-    }
-    try {
-      setLoading(true);
-      await sendPasswordResetEmail(auth, email);
-      setMessage('Check your email for password reset instructions.');
-      // Optionally redirect the user or provide further instructions
-    } catch (error) {
-      console.error('Error sending password reset email:', error);
-      setMessage('Failed to send password reset email. Please try again.');
     } finally {
       setLoading(false);
     }
@@ -154,57 +135,7 @@ const ResetPasswordForm: React.FC = () => {
     );
   }
 
-  return (
-    <div className="rounded-lg bg-white shadow max-w-lg w-full">
-      <div className="space-y-4 p-8 xl:p-16">
-        <div className="flex flex-col text-left">
-          <div className="mb-1 text-4xl font-bold">Reset password</div>
-          <div className="text-large text-gray-500">Enter your email</div>
-        </div>
-        <div className="flex items-center space-x-2">
-          <hr className="h-px flex-1 border-0 bg-gray-300" />
-        </div>
-
-        <form className="space-y-4" onSubmit={handleSendResetEmail}>
-          <div>
-            <label
-              htmlFor="email"
-              className="mb-2 block text-sm font-medium text-gray-900"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900"
-              placeholder="name@company.com"
-              required
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </div>
-          <Button
-            className="w-full"
-            disabled={loading}
-            type="submit"
-            loading={loading}
-          >
-            Send Reset Email
-          </Button>
-          {message && (
-            <p className="font-bold text-base text-gray-500">{message}</p>
-          )}
-          <p className="text-sm font-light text-gray-500">
-            Remembered your password?{' '}
-            <span className="text-blue-600 font-medium hover:underline">
-              <Link href="/login">Login</Link>
-            </span>
-          </p>
-        </form>
-      </div>
-    </div>
-  );
+  return null;
 };
 
 export default ResetPasswordForm;
--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -1,19 +1,11 @@
 // file: lib/firebase/config.ts
 import { initializeApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import {
-  initializeFirestore,
-  // setLogLevel,
-  connectFirestoreEmulator,
-} from 'firebase/firestore';
-
-const useEmulator = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === 'true';
+import { getAuth } from 'firebase/auth';
+import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: useEmulator
-    ? 'localhost'
-    : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_GCP_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SEND_NAME,
@@ -26,13 +18,5 @@ const auth = getAuth(app);
 const database = initializeFirestore(app, {
   experimentalForceLongPolling: true,
 });
-
-if (useEmulator) {
-  connectAuthEmulator(auth, 'http://localhost:9099');
-  connectFirestoreEmulator(database, 'localhost', 8080);
-  console.log('Using Firebase Emulators');
-}
-
-// setLogLevel('debug');
 
 export { app, auth, database, firebaseConfig };
