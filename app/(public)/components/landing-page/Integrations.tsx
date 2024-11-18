2b579fde35d2ae5f70fccd90ea9141be16ae2267
Vladislav Sorokin <vladislav@proxylink.co>
2024-11-18 20:20:05 +0000

    style: integrations

--- app/(public)/components/landing-page/Integrations.tsx
+++ app/(public)/components/landing-page/Integrations.tsx
@@ -16,9 +16,9 @@ const Integrations = () => {
         />
       </div>
 
-      <div className="max-w-[1080px] mx-auto relative flex flex-col items-center justify-center mt-28 pt-28  text-center z-10">
+      <div className="max-w-[1080px] mx-auto relative flex flex-col items-center justify-center md:mt-28 md:pt-28  text-center z-10">
         <SectionBadge title="Integrations" />
-        <h2 className="text-4xl md:text-5xl font-semibold text-gray-900 mt-3 text-center">
+        <h2 className="text-4xl md:text-5xl font-semibold text-gray-900 mt-3 text-center max-w-[600px]">
           Connect your subscription billing platform and CRM
         </h2>
         <p className="text-base text-gray-500 mt-3 max-w-prose text-center">
