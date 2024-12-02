732c43ef1bd6a1aae97f97de93a6cc9299a8e1bd
Vladislav Sorokin <vladislav@proxylink.co>
2024-12-02 16:41:45 +0000

    fix: cta btn

--- app/(public)/components/landing-page/CallToAction.tsx
+++ app/(public)/components/landing-page/CallToAction.tsx
@@ -5,13 +5,6 @@ import { FC } from 'react';
 const CallToAction: FC = () => {
   return (
     <div className="w-full p-6 md:max-w-[1080px] mx-auto relative flex flex-col items-center justify-center md:pt-24 pb-40 mt-20 md:mt-[280px]">
-      <div className="absolute inset-0 w-full h-full z-[1] md:-mt-24">
-        <img
-          src="/images/ready-bg.svg"
-          alt="Background"
-          className="hidden md:block object-contain w-full max-w-[1080px] mx-auto"
-        />
-      </div>
       <h2 className="text-4xl md:text-5xl font-semibold text-gray-900 mt-2 bg-landing text-center">
         Ready to get started?
       </h2>
@@ -28,6 +21,13 @@ const CallToAction: FC = () => {
         <Link href="/schedule-demo">
           <Button color="primary">Get Started for Free</Button>
         </Link>
+      </div>
+      <div className="absolute inset-0 w-full h-full z-[1] md:-mt-24 pointer-events-none">
+        <img
+          src="/images/ready-bg.svg"
+          alt="Background"
+          className="hidden md:block object-contain w-full max-w-[1080px] mx-auto"
+        />
       </div>
     </div>
   );
