458f593bbe020805535875e352523b8736ca5a38
Vladislav Sorokin <vladislav@proxylink.co>
2024-12-02 21:02:47 +0000

    feat: no hero image

--- app/(public)/components/landing-page/Hero.tsx
+++ app/(public)/components/landing-page/Hero.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 
 const LandingHero = () => {
   return (
-    <section className="p-6 md:p-0 w-full md:max-w-[1080px] mx-auto flex flex-col md:flex-row justify-between">
+    <section className="p-6 md:p-0 md:pb-12 w-full md:max-w-[1080px] mx-auto flex flex-col md:flex-row justify-between">
       <div className="w-full md:max-w-prose relative mt-10 md:mt-28">
         <h1 className="text-4xl leading-tight md:text-[56px] font-semibold text-gray-900 mb-6">
           Master Third Party Customer Experience
@@ -19,16 +18,6 @@ const LandingHero = () => {
             Get Started for Free
           </Button>
         </Link>
-      </div>
-      <div className="md:mt-4">
-        <Image
-          src="/images/hero-group.png"
-          width={332}
-          height={678}
-          alt="ProxyLink customers"
-          priority
-          className="w-full"
-        />
       </div>
     </section>
   );
