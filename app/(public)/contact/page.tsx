feb10c6076a4bfd6708719eb8c58e83252d10205
Vladislav Sorokin <vladislav@proxylink.co>
2024-12-03 15:05:52 +0000

    fix: space

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -1,11 +1,9 @@
 import ContactForm from '../components/ContactForm';
-import SectionBadge from '../components/landing-page/SectionBadge';
 
 export default function ContactPage() {
   return (
-    <div className="flex flex-col md:max-w-[1080px] mx-auto py-12 md:py-16 gap-8 md:gap-12 min-h-[100dvh] md:min-h-screen md:pt-32">
+    <div className="flex flex-col md:max-w-[1080px] mx-auto p-6 py-24 md:py-16 gap-8 md:gap-12 min-h-[100dvh] md:min-h-screen md:pt-32">
       <div className="w-full flex flex-col items-center justify-center">
-        <SectionBadge title="Contact Us" />
         <h2 className="text-4xl md:text-5xl font-semibold text-gray-900 mt-3 bg-landing text-center">
           Contact Our Team
         </h2>
@@ -13,7 +11,7 @@ export default function ContactPage() {
           Let us know how we can help
         </p>
       </div>
-      <div className="w-full flex justify-center gap-4 p-4 md:p-0">
+      <div className="w-full flex justify-center gap-4">
         <div className="w-full md:w-1/2 bg-white rounded-lg p-7">
           <h1 className="text-xl md:text-2xl mb-2 font-semibold">Contact Us</h1>
           <span className="text-sm text-gray-500">
--- app/(public)/schedule-demo/page.tsx
+++ app/(public)/schedule-demo/page.tsx
@@ -1,11 +1,9 @@
 import ContactForm from '../components/ContactForm';
-import SectionBadge from '../components/landing-page/SectionBadge';
 
 export default function ScheduleDemoPage() {
   return (
-    <div className="flex flex-col md:max-w-[1080px] mx-auto py-12 md:py-16 gap-8 md:gap-12 min-h-[100dvh] md:min-h-screen md:pt-32">
+    <div className="flex flex-col md:max-w-[1080px] mx-auto p-6 py-24 md:py-16 gap-8 md:gap-12 min-h-[100dvh] md:min-h-screen md:pt-32">
       <div className="w-full flex flex-col items-center justify-center">
-        <SectionBadge title="Contact Us" />
         <h2 className="text-4xl md:text-5xl font-semibold text-gray-900 mt-3 bg-landing text-center">
           Schedule a demo
         </h2>
@@ -13,8 +11,8 @@ export default function ScheduleDemoPage() {
           Let us know how we can help
         </p>
       </div>
-      <div className="w-full flex flex-col md:flex-row gap-4 p-4 md:p-0">
-        <div className="w-full md:w-1/2 bg-white rounded-lg p-4 md:p-7 md:self-start">
+      <div className="w-full flex flex-col md:flex-row gap-4 md:p-0">
+        <div className="w-full md:w-1/2 bg-white rounded-lg p-7 md:self-start">
           <h1 className="text-xl md:text-2xl mb-2 font-semibold">
             See ProxyLink in Action
           </h1>
